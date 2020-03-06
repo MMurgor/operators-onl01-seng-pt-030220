7a67f46e6ec695487f78671ebbd558e0f20094ea
Romena Murgor <romenamurgor@gmail.com>
2020-03-06 17:38:09 +0000

    Automatically backed up by Learn

--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -3,7 +3,7 @@ describe "Operations" do
 
   describe "#unsafe?" do
     it 'returns true if speed is greater than 60' do
-      answer = unsafe?(89)
+      answer = unsafe?(79)
       expect(answer).to eq(true)
     end
 
